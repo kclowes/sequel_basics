bf026f304e133ebec7f4851909c0198b5622a0ab
Evan Tedesco <Evantedesco@gmail.com>
2014-04-04 08:32:49 -0600

    ALlows user to edit items via the id

--- lib/task_repostitory.rb
+++ lib/task_repostitory.rb
@@ -15,5 +15,15 @@ class TaskRepository
     tasks = @db[:tasks]
     tasks.all
   end
+
+  def update(update_id ,update_info)
+    tasks = @db[:tasks]
+    tasks.where(:id => update_id).update(update_info)
+  end
 end
+
+#Updating the attributes of an existing task. This method will take a hash of
+#the new attribute values. For example, if you only want to update the
+#completed attribute, you would pass in {:completed => true}. If you want
+#to update both, you would pass in {:name => "New Name", :completed => true} end
 
--- spec/task_repository_spec.rb
+++ spec/task_repository_spec.rb
@@ -14,7 +14,23 @@ describe "Manages tasks" do
 
     tasks.add_task({:id => 1, :task => 'do_stuff'})
     tasks.add_task({:id => 2, :task => 'do_more_stuff'})
-    actual = [{:id => 1, :task => 'do_stuff', :completed => false}, {:id => 2, :task => 'do_more_stuff', :completed => false}]
-    expect(tasks.show_all).to eq(actual)
+    expected = [{:id => 1, :task => 'do_stuff', :completed => false}, {:id => 2, :task => 'do_more_stuff', :completed => false}]
+    expect(tasks.show_all).to eq(expected)
+  end
+
+  it "Allow the user to update valuen in the table" do
+    db = Sequel.connect('postgres://gschool_user:password@localhost:5432/tasks_test')
+    tasks = TaskRepository.new(db)
+    db.create_table! :tasks do
+      primary_key :id
+      String :task
+      BOOLEAN :completed, :default => false
+    end
+    tasks.add_task({:id => 1, :task => 'do_stuff'})
+    tasks.add_task({:id => 2, :task => 'do_more_stuff'})
+    tasks.update(1,{:task => 'do lots of stuff', :completed => true})
+    expected = [{:id => 2, :task => 'do_more_stuff', :completed => false},{:id => 1, :task => 'do lots of stuff', :completed => true}]
+    expect(tasks.show_all).to eq(expected)
+
   end
 end
